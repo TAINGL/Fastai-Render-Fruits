b5abe8c3b97f954bceab377afdc1f42eb2268717
Laura Taing <laura.taing@hotmail.com>
2020-07-02 15:11:01 +0200

    first commit

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=15o0kql8K4gvkGPxYoHn49HK4iAIA6BTg'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['litchi','rambutan','longan']
 path = Path(__file__).parent
 
 app = Starlette()
